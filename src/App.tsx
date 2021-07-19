af231ccc176de9a8e2a935199596e3f54c3973b0
Joe Trellick <joe@trellick.work>
2021-07-19 18:23:41 +0100

    Add a basic play/stop toggle using hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+function Player() {
+  const [playing, setPlaying] = useState(false);
+
+  const buttonText = playing ? 'Stop' : 'Play';
+
+  return (
+    <div id="player">
+      <button onClick={() => setPlaying(!playing)}>
+        {buttonText}
+      </button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Player />
       </header>
     </div>
   );
