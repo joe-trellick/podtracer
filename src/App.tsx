97a0a319848d7ec13ddba5595f9619899666ce49
Joe Trellick <joe@trellick.work>
2021-07-19 19:09:07 +0100

    Hook up the play/stop button
    Temporarily plays psytrance music from https://www.psychedelik.com/

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var audio: HTMLAudioElement | null;
+
 function Player() {
   const [playing, setPlaying] = useState(false);
+  const [audioUrl, setAudioUrl] = useState('https://stream.psychedelik.com:8000/listen.mp3');
+
+  useEffect(() => {
+    console.log(`set audioUrl to ${audioUrl}`);
+    audio = new Audio(audioUrl);
+  }, [audioUrl]);
+
+  useEffect(() => {
+    console.log(`playing is now ${playing}`);
+    if (playing) {
+      audio?.play();
+    } else {
+      audio?.pause();
+    }
+  }, [playing]);
 
   const buttonText = playing ? 'Stop' : 'Play';
 
